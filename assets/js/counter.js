c5d398cb43d79625d333809c49c32990034f4633
phongku <etrin00azn@gmail.com>
2023-06-13 10:41:56 -0700

    Delete assets directory

--- assets/js/counter.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://yjhbqwu4sersl2gpv7vgc2ubum0eabxl.lambda-url.us-west-2.on.aws/")
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
-}
-
-updateCounter();
